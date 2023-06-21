f05afdccf289a7c7e2348415db58bf834fcf7467
Gabriel Matos <gabrielmatos@unifor.br>
2023-06-21 09:01:12 -0300

    Controller e primeiros objetos

---
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -0,0 +1,5 @@
+package med.voll.api.endereco;
+
+public record DadosEndereco(String logrdouro, String bairro, String cep, String cidade, String uf, String complemento,
+                            String numero) {
+}
