6a1702454579a4590bddc474a70b6094cd621b16
Gabriel Matos <gabrielmatos@unifor.br>
2023-06-21 10:43:07 -0300

    Criando e Mapeando JPA

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -18,4 +18,14 @@ public class Endereco {
     private String uf;
     private String complemento;
     private String numero;
+
+    public Endereco(DadosEndereco dados) {
+        this.logrdouro = dados.logrdouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+        this.complemento = dados.complemento();
+        this.numero = dados.numero();
+    }
 }
