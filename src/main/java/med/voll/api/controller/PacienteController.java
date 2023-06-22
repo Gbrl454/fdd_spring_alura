a06d0d1a38c1b59d4fbdd94dadce3135e5d096b7
Gabriel Matos <gabrielmatos@unifor.br>
2023-06-22 11:41:59 -0300

    Lidando com erros

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,7 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.paciente.*;
+import med.voll.api.domain.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,25 +18,25 @@ public class PacienteController {
 
     @PostMapping
     @Transactional
-    public void cadastrar (@RequestBody @Valid DadosCadastroPaciente dados) {
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
     }
 
     @GetMapping
-    public Page<DadosListagemPaciente> listar (@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao) {
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao) {
         return repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar (@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
         var paciente = repository.getReferenceById(dados.id());
         paciente.atuzalizarInformacoes(dados);
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void deletar (@PathVariable Long id) {
+    public void deletar(@PathVariable Long id) {
         var paciente = repository.getReferenceById(id);
         paciente.excluir();
     }
--- src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -1,19 +1,19 @@
-package med.voll.api.medico;
+package med.voll.api.domain.paciente;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import med.voll.api.endereco.Endereco;
+import med.voll.api.domain.endereco.Endereco;
 
-@Table(name = "medicos")
-@Entity(name = "Medico")
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Medico {
+public class Paciente {
     // JPA
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,27 +22,23 @@ public class Medico {
     private String nome;
     private String email;
     private String telefone;
-    private String crm;
-
-    @Enumerated(EnumType.STRING)
-    private Especialidade especialidade;
+    private String cpf;
 
     @Embedded
     private Endereco endereco;
 
     private Boolean ativo;
 
-    public Medico (DadosCadastroMedico dados) {
+    public Paciente (DadosCadastroPaciente dados) {
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
-        this.crm = dados.crm();
-        this.especialidade = dados.especialidade();
+        this.cpf = dados.cpf();
         this.endereco = new Endereco(dados.endereco());
         this.ativo = true;
     }
 
-    public void atuzalizarInformacoes (DadosAtualizacaoMedico dados) {
+    public void atuzalizarInformacoes (DadosAtualizacaoPaciente dados) {
         this.nome = (dados.nome() != null) ? dados.nome() : this.nome;
         this.telefone = (dados.telefone() != null) ? dados.telefone() : this.telefone;
         if (dados.endereco() != null) this.endereco.atuzalizarInformacoes(dados.endereco());
