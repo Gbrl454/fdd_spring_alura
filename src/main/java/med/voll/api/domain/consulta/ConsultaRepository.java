f3fc9453bf3e624415614dcfd65b786ce6a9fa34
951549700@adm.unifor.br <gabrielmatos@unifor.br>
2023-06-29 15:10:05 -0300

    Regras de Negócio

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
-    boolean existsByMadicoIdAndData (Long id, LocalDateTime data);
+    boolean existsByMedicoIdAndData (Long id, LocalDateTime data);
 
     boolean existsByPacienteIdAndDataBetween (Long id, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
 }
