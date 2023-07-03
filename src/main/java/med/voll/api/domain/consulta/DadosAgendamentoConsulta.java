1cb12a0d6b3376d3cbf31dbc992306a9053973ca
951549700@adm.unifor.br <gabrielmatos@unifor.br>
2023-07-03 10:19:52 -0300

    Validação Consulta

--- src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -7,7 +7,7 @@ import med.voll.api.domain.medico.Especialidade;
 
 import java.time.LocalDateTime;
 
-public record DadosAgendamentoConsulta(Long idMedico, @NotNull Long idPaciente,
-                                       @NotNull @Future @JsonFormat(pattern = "dd/MM/yyyy HH:mm") LocalDateTime data,
+public record DadosAgendamentoConsulta(Long idMedico, @NotNull(message = "{paciente.obrigatorio}") Long idPaciente,
+                                       @NotNull(message = "{data.obrigatorio}") @Future @JsonFormat(pattern = "dd/MM/yyyy HH:mm") LocalDateTime data,
                                        Especialidade especialidade) {
 }
